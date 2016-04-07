4abc629d46fadec2d1f16c69361cb2dd591b09db
David-Mikek <david.mikek@gmail.com>
2016-04-07 13:24:01 +0200

    upgrade
    še mal spremenjeno

--- zajec_skupina4/message.php
+++ zajec_skupina4/message.php
@@ -1,14 +1,17 @@
 <?php
 
 if($_POST["submit"]) {
-	//to bi naj delovalo nekako tako: če gre obrazec naprej,se naj to izvrši
-    $recipient="your@email.address";
-    $subject="Form to email message";
+	//to bi naj delovalo nekako tako: če gre obrazec naprej,se naj to izvrši. o
+    $recipient=$_POST["recieverEmail"];
+    $subject=$_POST["subject"];
     $sender=$_POST["sender"];
     $senderEmail=$_POST["senderEmail"];
     $message=$_POST["message"];
 
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+    $mailBody="Od: $sender\nKontakt: $senderEmail\n\n$message";
+
+   // klasični email obrazec baje zgleda tako
+  // mail(prejemnik,zadeva,sporočilo,$headers);
 
     mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
 
@@ -34,11 +37,19 @@ if($_POST["submit"]) {
         <label>Ime:</label>
         <input name="sender">
 
-        <label>Email address:</label>
+        <label>Vaš Email:</label>
         <input name="senderEmail">
 
+		<label>Prejemnikov Email:</label>
+        <input name="recieverEmail">
+		
+		<label>Zadeva:</label>
+        <input name="subject">
+		
         <label>Sporočilo:</label>
         <textarea rows="5" cols="20" name="message"></textarea>
+		
+	//obrazec pošlje podatke na isto stran,oziroma v php nad doctypu	
 
         <input type="submit" name="submit">
     </form>
@@ -46,9 +57,6 @@ if($_POST["submit"]) {
 </body>
 
 </html>
-// klasični email obrazec naj zgleda tako
-// mail(prejemnik,zadeva,sporočilo,$headers);
-
 //pa še footer za konec
 <?php include_once 'footer.php';
 ?>
